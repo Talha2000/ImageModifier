21200be7ca8eae4341fb54bac8189d75f9bc0acd
Talha2000 <amjad_maqsood90@hotmail.com>
2022-01-04 03:49:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageFilter
+from PIL.ImageFilter import (
+     BLUR, CONTOUR, EDGE_ENHANCE, EDGE_ENHANCE_MORE,
+     EMBOSS, FIND_EDGES, SHARPEN
+)
+import sys
+        
+def Test(imageFileName):
+    return imageFileName
+
+
+print(Test)
