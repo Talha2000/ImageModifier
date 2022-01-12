17e4c83c95a325fa9c2ae2346482fea4910272e2
Talha2000 <amjad_maqsood90@hotmail.com>
2022-01-12 14:05:43 -0500

    Pushing changes

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ from PIL.ImageFilter import (
      EMBOSS, FIND_EDGES, SHARPEN
 )
 import sys
+
+class Image:       
+    def __init__():
         
-def Test(imageFileName):
-    return imageFileName
 
 
 print(Test)
+
+
+
+# from PIL import Image, ImageFilter
+
+# img.convert('L')
+
+
+# img = Image.open('astro.jpg')
+# img.thumbnail((400,400))
+# img.save('thumbnail.jpg')
+# img.show()
+
+# crooked = filtered_img.rotate(180)
+# crooked.save("grey.png", 'png')
+# crooked.show()
+# box = (100, 100, 400, 400)
+# region = filtered_img.crop(box)
+# region.save("grey.png", 'png')
+# region.show()
+
